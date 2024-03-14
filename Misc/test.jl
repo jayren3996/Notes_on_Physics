768eb4764e040c561c9bd3cb3b85fa8e58258960
JieRen <32599461+jayren3996@users.noreply.github.com>
2024-03-14 13:50:05 +0800

    update

---
+++ Misc/test.jl
@@ -0,0 +1,61 @@
+### A Pluto.jl notebook ###
+# v0.19.37
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 81a22226-be58-11ee-3f3c-dd3cdfd58dc7
+using LinearAlgebra
+
+# ╔═╡ d587be00-9bef-4479-8410-a9087f8bf832
+a = 1
+
+# ╔═╡ 2de7603f-4b15-43ab-b774-859452c60207
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+LinearAlgebra = "37e2e46d-f89d-539d-b4ee-838fcccc9c8e"
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.9.0"
+manifest_format = "2.0"
+project_hash = "ac1187e548c6ab173ac57d4e72da1620216bce54"
+
+[[deps.Artifacts]]
+uuid = "56f22d72-fd6d-98f1-02f0-08ddc0907c33"
+
+[[deps.CompilerSupportLibraries_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "e66e0078-7015-5450-92f7-15fbd957f2ae"
+version = "1.0.2+0"
+
+[[deps.Libdl]]
+uuid = "8f399da3-3557-5675-b5ff-fb832c97cbdb"
+
+[[deps.LinearAlgebra]]
+deps = ["Libdl", "OpenBLAS_jll", "libblastrampoline_jll"]
+uuid = "37e2e46d-f89d-539d-b4ee-838fcccc9c8e"
+
+[[deps.OpenBLAS_jll]]
+deps = ["Artifacts", "CompilerSupportLibraries_jll", "Libdl"]
+uuid = "4536629a-c528-5b80-bd46-f80d51c5b363"
+version = "0.3.21+4"
+
+[[deps.libblastrampoline_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "8e850b90-86db-534c-a0d3-1478176c7d93"
+version = "5.7.0+0"
+"""
+
+# ╔═╡ Cell order:
+# ╠═81a22226-be58-11ee-3f3c-dd3cdfd58dc7
+# ╠═d587be00-9bef-4479-8410-a9087f8bf832
+# ╠═2de7603f-4b15-43ab-b774-859452c60207
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
